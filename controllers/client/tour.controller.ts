99cfa2e19e0c84fdb5e821fd7e9412abd0689b78
Nguyen Minh Hoang <nemcabong@gmail.com>
2023-12-19 17:46:06 +0700

    Tao controller

---
+++ controllers/client/tour.controller.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from "express";
+import Tour from "../../models/tour.model";
+
+// [GET] /tours
+export const index = async (req: Request, res: Response) => {
+  const tours = await Tour.findAll({
+    where: {
+      deleted: false,
+      status: "active",
+    },
+    raw: true,
+  });
+
+  console.log(tours);
+
+  res.render("client/pages/tours/index", {
+    pageTitle: "Danh sách tour",
+    tours: tours,
+  });
+};
