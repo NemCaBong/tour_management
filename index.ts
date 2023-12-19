ae030fb0868982aa761f15674cc413d30f912e23
Nguyen Minh Hoang <nemcabong@gmail.com>
2023-12-19 16:42:44 +0700

    Cau hinh pug

--- index.ts
+++ index.ts
@@ -3,8 +3,15 @@ import express, { Express, Request, Response } from "express";
 const app: Express = express();
 const port: number = 3000;
 
+app.set("views", "./views");
+app.use("view engine", "pug");
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Trang chủ");
+});
+
+app.get("/tours", (req: Request, res: Response) => {
+  res.render("client/pages/tours/index");
 });
 
 app.listen(port, () => {
